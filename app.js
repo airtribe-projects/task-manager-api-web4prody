fb95f0ef1159e9f8f0643cf4bb243b7f78f148bf
web4prody <prodyut.paul86@gmail.com>
2026-07-31 22:06:46 +0530

    Initialize Express server in app.js
    Set up an Express server with JSON and URL-encoded body parsing.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-module.exports = require('./src/app');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('Something bad happened', err);
+    }
+    console.log(`Server is listening on ${port}`);
+});
+
+
+
+module.exports = app;
